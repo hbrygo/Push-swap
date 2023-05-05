bae0e20631f5d6e55796f724dfcec35a7ff6ad8b
Hugo Brygo <hubrygo@shi-r10-p2.s19.be>
2023-05-05 13:57:09 +0200

    debut des fonctions de base

--- ft_invert_first.c
+++ ft_invert_first.c
@@ -6,24 +6,36 @@
 /*   By: hubrygo <hubrygo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 10:31:51 by hubrygo           #+#    #+#             */
-/*   Updated: 2023/05/05 10:08:38 by hubrygo          ###   ########.fr       */
+/*   Updated: 2023/05/05 11:50:42 by hubrygo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_sa()
+void	*ft_sa(t_list lst)
 {
-	
+	int	temp;
+
+	if (lst.content == NULL || lst.next == NULL)
+		return (0);
+	temp = lst.content;
+	lst.content = lst.next;
+	lst.next = temp;
 }
 
-int	ft_sb()
+void	*ft_sb(t_list lst)
 {
+	int	temp;
 
+	if (lst.content == NULL || lst.next == NULL)
+		return (0);
+	temp = lst.content;
+	lst.content = lst.next;
+	lst.next = temp;
 }
 
-int	ft_ss()
+void	*ft_ss(t_list *lst_a, t_list *lst_b)
 {
-	ft_sa();
-	ft_sb();
+	ft_sa(lst_a);
+	ft_sb(lst_b);
 }
--- ft_set_lst.c
+++ ft_set_lst.c
@@ -6,7 +6,7 @@
 /*   By: hubrygo <hubrygo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 10:06:27 by hubrygo           #+#    #+#             */
-/*   Updated: 2023/05/05 11:24:56 by hubrygo          ###   ########.fr       */
+/*   Updated: 2023/05/05 11:38:24 by hubrygo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,20 @@ t_list	*ft_set_lst(int argc, char **argv)
 	int		i;
 	t_list	*lst;
 	t_list	*temp;
-	int		test;
 
-	lst = ft_lstnew(ft_atoi(argv[1]));
+	if (argc == 1)
+		return (NULL);
+	if (argc == 2)
+	{
+		lst = ft_lstsplit(argv[1]);
+		return (lst);
+	}
+	else
+		lst = ft_lstnew(ft_atoi(argv[1]));
 	i = 1;
 	while (++i < argc)
 	{
-		test = ft_atoi(argv[i]);
-		temp = ft_lstnew(test);
+		temp = ft_lstnew(ft_atoi(argv[i]));
 		ft_lstadd_back(&lst, temp);
 	}
 	return (lst);
